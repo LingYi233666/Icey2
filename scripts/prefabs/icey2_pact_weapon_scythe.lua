b2746143677ff836a831ff49a0d67c3bcf75156f
Cheng Jie <g.cheng01@smart.com>
2024-12-05 18:31:45 +0800

    feat: add scythe projectile code.

--- scripts/prefabs/icey2_pact_weapon_scythe.lua
+++ scripts/prefabs/icey2_pact_weapon_scythe.lua
@@ -13,6 +13,9 @@ local function onequip(inst, owner)
     owner.AnimState:Hide("ARM_normal")
 
     owner.AnimState:SetSymbolLightOverride("swap_object", 0.6)
+
+    inst.components.icey2_bonus_area:Stop()
+    inst:RemoveTag("icey2_pact_weapon_no_regive")
 end
 
 local function onunequip(inst, owner)
@@ -61,6 +64,68 @@ local function DoScythe(inst, doer, target)
 
     return true
 end
+
+local function ProjectileOnThrown(inst, thrower)
+    inst.launch_time = GetTime()
+
+    -- inst.AnimState:PlayAnimation("spin_loop", true)
+    inst.AnimState:SetPercent("height_controller", 200 / 1000)
+
+    inst.rolling_fx = inst:SpawnChild("icey2_pact_weapon_scythe_rolling")
+    if not inst.rolling_fx.Follower then
+        inst.rolling_fx.entity:AddFollower()
+    end
+    inst.rolling_fx:FollowSymbol(inst.GUID, "swap_rolling_fx", 0, 0, 0)
+
+    inst.SoundEmitter:PlaySound("wilson_rework/torch/torch_spin", "spin_loop")
+
+    inst.components.inventoryitem.canbepickedup = false
+
+
+    inst:AddTag("FX")
+    inst:AddTag("icey2_pact_weapon_no_regive")
+end
+
+local function ProjectileOnUpdate(inst)
+    local dt = FRAMES
+    local speed = inst.components.complexprojectile.horizontalSpeed
+    inst.Physics:SetMotorVel(speed, 0, 0)
+
+    if GetTime() - inst.launch_time > 1 then
+        inst.components.complexprojectile:Hit()
+        return
+    end
+
+    -- TODO: Hit enemies
+end
+
+local function ProjectileOnHit(inst)
+    if inst.rolling_fx and inst.rolling_fx:IsValid() then
+        inst.rolling_fx:Remove()
+    end
+    inst.rolling_fx = nil
+
+    inst.AnimState:PlayAnimation("idle")
+
+    inst.SoundEmitter:KillSound("spin_loop")
+    inst.SoundEmitter:PlaySound("wilson_rework/torch/stick_ground")
+
+    inst.components.inventoryitem.canbepickedup = true
+
+    -- TODO: Spawn fxs, hit enemies
+    SpawnAt("explode_small", inst)
+
+    -- Bonus area start
+    inst.components.icey2_bonus_area:Start(10)
+
+    inst:RemoveTag("FX")
+end
+
+local function BonusAreaTest(inst, target)
+    return target.components.combat
+        and target == inst.owner
+end
+
 
 local function OnSpellHit(inst, doer, target)
 
@@ -114,9 +179,19 @@ local function fn()
     inst.components.equippable:SetOnEquip(onequip)
     inst.components.equippable:SetOnUnequip(onunequip)
 
-
     inst:AddComponent("icey2_scythe")
     inst.components.icey2_scythe:SetDoScytheFn(DoScythe)
+
+    inst:AddComponent("complexprojectile")
+    inst.components.complexprojectile:SetOnLaunch(ProjectileOnThrown)
+    inst.components.complexprojectile:SetOnUpdate(ProjectileOnUpdate)
+    inst.components.complexprojectile:SetOnHit(ProjectileOnHit)
+    inst.components.complexprojectile.ismeleeweapon = true
+
+    inst:AddComponent("icey2_bonus_area")
+    inst.components.icey2_bonus_area.radius = 6
+    inst.components.icey2_bonus_area.circle_prefab = "icey2_circle_mark_iceyblue_6"
+    inst.components.icey2_bonus_area.testfn = BonusAreaTest
 
     -- inst:AddComponent("icey2_aoeweapon_flurry_lunge")
     -- inst.components.icey2_aoeweapon_flurry_lunge:SetOnHitFn(OnSpellHit)
@@ -129,106 +204,19 @@ local function fn()
     return inst
 end
 
-----------------------------------------------------------------
-local buff_radius = 6
-
-local function CanBeBuffered(inst, target)
-    return target
-        and target:IsValid()
-        and target.components.combat
-        and target == inst.owner
-        and target:IsNear(inst, buff_radius)
-end
-
-local function BufferTask(inst)
-    local x, y, z = inst.Transform:GetWorldPosition()
-    local ents = TheSim:FindEntities(x, y, z, buff_radius, { "_combat" }, { "INLIMBO" })
-
-    for _, v in pairs(ents) do
-        if inst.buffered_creatures[v] == nil and CanBeBuffered(inst, v) then
-            -- Buffer target
-            -- v.components.combat.externaldamagemultipliers:SetModifier(inst, 1.1, inst.prefab)
-            if not v.components.planardamage then
-                v:AddComponent("planardamage")
-            end
-            v.components.planardamage:AddBonus(inst, 5, inst.prefab)
-
-            inst.buffered_creatures[v] = true
-        end
-    end
-
-    local ents_to_be_removed = {}
-
-    for ent, _ in pairs(inst.buffered_creatures) do
-        if not CanBeBuffered(inst, ent) then
-            table.insert(ents_to_be_removed, ent)
-        end
-    end
-
-    for _, v in pairs(ents_to_be_removed) do
-        if v:IsValid() then
-            -- Remove buffer of target
-            -- v.components.combat.externaldamagemultipliers:RemoveModifier(inst, inst.prefab)
-            v.components.planardamage:RemoveBonus(inst, inst.prefab)
-        end
-        inst.buffered_creatures[v] = nil
-    end
-end
-
-
-local function KillFX(inst)
-    if inst.components.timer:TimerExists("killfx") then
-        inst.components.timer:StopTimer("killfx")
-    end
-
-    if inst.buffer_task then
-        inst.buffer_task:Cancel()
-        inst.buffer_task = nil
-    end
-
-    -- inst.AnimState:PlayAnimation("")
-    -- inst:ListenForEvent("animover", inst.Remove)
-
-    inst:Remove()
-end
-
-local function SetOwner(inst, owner)
-    inst.owner = owner
-
-    inst._on_owner_invalid = function()
-        inst:RemoveEventCallback("onremove", inst._on_owner_invalid, owner)
-        inst:RemoveEventCallback("playerdeactivated", inst._on_owner_invalid, owner)
-        inst:RemoveEventCallback("death", inst._on_owner_invalid, owner)
-
-        inst:KillFX()
-    end
-
-    inst:ListenForEvent("onremove", inst._on_owner_invalid, owner)
-    inst:ListenForEvent("playerdeactivated", inst._on_owner_invalid, owner)
-    inst:ListenForEvent("death", inst._on_owner_invalid, owner)
-end
-
-local function TotemTimerDone(inst, data)
-    if data.name == "killfx" then
-        inst:KillFX()
-    end
-end
-
-local function totemfn()
+local function height_controller_fn()
     local inst = CreateEntity()
 
     inst.entity:AddTransform()
     inst.entity:AddAnimState()
     inst.entity:AddNetwork()
 
-    MakeInventoryPhysics(inst)
-    RemovePhysicsColliders(inst)
+    MakeProjectilePhysics(inst)
 
     inst.AnimState:SetBank("icey2_pact_weapon_scythe")
     inst.AnimState:SetBuild("icey2_pact_weapon_scythe")
-    inst.AnimState:PlayAnimation("totem")
+    -- inst.AnimState:PlayAnimation("controller")
 
-    inst:AddTag("NOCLICK")
     inst:AddTag("FX")
 
     inst.entity:SetPristine()
@@ -237,30 +225,45 @@ local function totemfn()
         return inst
     end
 
+    inst.SetHeight = function(inst, height_pixel)
+        local max_height_pixel = 1000
+        inst.AnimState:SetPercent("controller", height_pixel / max_height_pixel)
+    end
+
+
+    return inst
+end
+
+local function rollingfn()
+    local inst = CreateEntity()
+
+    inst.entity:AddTransform()
+    inst.entity:AddAnimState()
+    inst.entity:AddNetwork()
+
+    MakeProjectilePhysics(inst)
+
+    inst.Transform:SetEightFaced()
+
+    inst.AnimState:SetBank("icey2_pact_weapon_scythe")
+    inst.AnimState:SetBuild("icey2_pact_weapon_scythe")
+    inst.AnimState:PlayAnimation("rolling", true)
+
+    inst:AddTag("FX")
+    inst:AddTag("NOCLICK")
+
+    inst.entity:SetPristine()
+
+    if not TheWorld.ismastersim then
+        return inst
+    end
+
+
     inst.persists = false
-
-    inst.buffered_creatures = {}
-    inst.buffer_task = inst:DoPeriodicTask(0, BufferTask)
-
-
-    inst.KillFX = KillFX
-    inst.SetOwner = SetOwner
-
-
-    -- May be do damage to nearby enemies ?
-    -- inst:AddComponent("weapon")
-    -- inst.components.weapon:SetDamage(17)
-
-    -- inst:AddComponent("icey2_spdamage_force")
-    -- inst.components.icey2_spdamage_force:SetBaseDamage(17)
-
-    inst:AddComponent("timer")
-    inst.components.timer:StartTimer("killfx", 10)
-
-    inst:ListenForEvent("timerdone", TotemTimerDone)
 
     return inst
 end
 
 return Prefab("icey2_pact_weapon_scythe", fn, assets),
-    Prefab("icey2_pact_weapon_scythe_totem", totemfn, assets)
+    -- Prefab("icey2_pact_weapon_scythe_height_controller", height_controller_fn, assets)
+    Prefab("icey2_pact_weapon_scythe_rolling", rollingfn, assets)
