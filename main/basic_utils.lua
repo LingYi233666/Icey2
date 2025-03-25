a5609aec3b07003902fd4399facbe3649c352f15
程捷 <1426163582@qq.com>
2025-03-25 21:45:01 +0800

    fix: armorskeleton is treat as armor

--- main/basic_utils.lua
+++ main/basic_utils.lua
@@ -2,8 +2,8 @@ Icey2Basic = {}
 
 function Icey2Basic.IsWearingArmor(inst)
     for k, v in pairs(inst.components.inventory.equipslots) do
-        if v.components.armor ~= nil and
-            not v:HasTag("ignore_icey2_unarmoured_defence_limit") then
+        if (v.components.armor ~= nil or v.components.resistance ~= nil)
+            and not v:HasTag("ignore_icey2_unarmoured_defence_limit") then
             return true
         end
     end
